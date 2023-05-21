239cbf8fdab91147b3f02c02c26cfa494424871f
jay-brew <brew0@naver.com>
2023-05-21 21:09:11 +0900

    desktop - routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppRouter from './AppRouter';
+import {createRoot} from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.render 를 사용하는 경우 React 18 이상 버전에서 경고 브라우저 콘솔에 뜬다.
+// createRoot 로 대체 (경고 뜨지 않는 경우 사용해도 무관)
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<AppRouter tab="home" />);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+//);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
