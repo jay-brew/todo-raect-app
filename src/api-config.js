5b2b2e03ec471d4c8eac989ab23734d257e7d31f
jay-brew <brew0@naver.com>
2023-05-29 03:47:48 +0900

    commit for deployment

--- src/api-config.js
+++ src/api-config.js
@@ -4,6 +4,9 @@ const hostname = window && window.location && window.location.hostname;
 
 if(hostname === "localhost"){
     backendHost = "http://localhost:8080";
+} else {
+    // <일랙스틱 빈스톡 애플리케이션 URL>
+    backendHost = "http://prod-demo-api-service.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
